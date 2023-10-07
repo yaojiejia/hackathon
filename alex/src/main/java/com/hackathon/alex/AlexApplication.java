1b3ac0c81866b3f8f109b32c5bfecbcb6c4932ed
yaojiejia <70050131+yaojiejia@users.noreply.github.com>
2023-10-07 11:31:09 -0400

    Add files via upload

---
+++ alex/src/main/java/com/hackathon/alex/AlexApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexApplication.class, args);
+	}
+
+}
