c9fd37dd90b3572e53366aecae16b5fb39c0a307
connor lee <connorlee184@gmail.com>
2023-10-07 15:17:40 -0400

    Fixed

--- nyu_connect/src/index.js
+++ nyu_connect/src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
-import Profile from "./pages/TeacherProfile";
+import TeacherProfile from "./pages/TeacherProfile";
 
 export default function App() {
   return (
@@ -10,7 +10,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}> 
           <Route index element={<Home />} />
-          <Route path="profile" element={<Profile />} />
+          <Route path="profile" element={<TeacherProfile />} />
+          <Route path="home" element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ nyu_connect/src/pages/Layout.js
@@ -0,0 +1,23 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/profile">TeacherProfile</Link>
+          </li>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+};
+
+export default Layout;
